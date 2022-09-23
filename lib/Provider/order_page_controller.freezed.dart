58aca21436418bc217352bea711f7f9e0489d741
taiga-mac <fujimurataiga@icloud.com>
2022-09-24 00:15:17 +0900

    発注数提案ページの状態管理を変更

---
+++ lib/Provider/order_page_controller.freezed.dart
@@ -0,0 +1,305 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'order_page_controller.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$OrderPageState {
+  Sales get todaySales => throw _privateConstructorUsedError;
+  Sales get tomorrowSales => throw _privateConstructorUsedError;
+  Sales get dayAfterSales => throw _privateConstructorUsedError;
+  Item get pork => throw _privateConstructorUsedError;
+  Item get beef => throw _privateConstructorUsedError;
+  Item get chicken => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $OrderPageStateCopyWith<OrderPageState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $OrderPageStateCopyWith<$Res> {
+  factory $OrderPageStateCopyWith(
+          OrderPageState value, $Res Function(OrderPageState) then) =
+      _$OrderPageStateCopyWithImpl<$Res>;
+  $Res call(
+      {Sales todaySales,
+      Sales tomorrowSales,
+      Sales dayAfterSales,
+      Item pork,
+      Item beef,
+      Item chicken});
+
+  $SalesCopyWith<$Res> get todaySales;
+  $SalesCopyWith<$Res> get tomorrowSales;
+  $SalesCopyWith<$Res> get dayAfterSales;
+  $ItemCopyWith<$Res> get pork;
+  $ItemCopyWith<$Res> get beef;
+  $ItemCopyWith<$Res> get chicken;
+}
+
+/// @nodoc
+class _$OrderPageStateCopyWithImpl<$Res>
+    implements $OrderPageStateCopyWith<$Res> {
+  _$OrderPageStateCopyWithImpl(this._value, this._then);
+
+  final OrderPageState _value;
+  // ignore: unused_field
+  final $Res Function(OrderPageState) _then;
+
+  @override
+  $Res call({
+    Object? todaySales = freezed,
+    Object? tomorrowSales = freezed,
+    Object? dayAfterSales = freezed,
+    Object? pork = freezed,
+    Object? beef = freezed,
+    Object? chicken = freezed,
+  }) {
+    return _then(_value.copyWith(
+      todaySales: todaySales == freezed
+          ? _value.todaySales
+          : todaySales // ignore: cast_nullable_to_non_nullable
+              as Sales,
+      tomorrowSales: tomorrowSales == freezed
+          ? _value.tomorrowSales
+          : tomorrowSales // ignore: cast_nullable_to_non_nullable
+              as Sales,
+      dayAfterSales: dayAfterSales == freezed
+          ? _value.dayAfterSales
+          : dayAfterSales // ignore: cast_nullable_to_non_nullable
+              as Sales,
+      pork: pork == freezed
+          ? _value.pork
+          : pork // ignore: cast_nullable_to_non_nullable
+              as Item,
+      beef: beef == freezed
+          ? _value.beef
+          : beef // ignore: cast_nullable_to_non_nullable
+              as Item,
+      chicken: chicken == freezed
+          ? _value.chicken
+          : chicken // ignore: cast_nullable_to_non_nullable
+              as Item,
+    ));
+  }
+
+  @override
+  $SalesCopyWith<$Res> get todaySales {
+    return $SalesCopyWith<$Res>(_value.todaySales, (value) {
+      return _then(_value.copyWith(todaySales: value));
+    });
+  }
+
+  @override
+  $SalesCopyWith<$Res> get tomorrowSales {
+    return $SalesCopyWith<$Res>(_value.tomorrowSales, (value) {
+      return _then(_value.copyWith(tomorrowSales: value));
+    });
+  }
+
+  @override
+  $SalesCopyWith<$Res> get dayAfterSales {
+    return $SalesCopyWith<$Res>(_value.dayAfterSales, (value) {
+      return _then(_value.copyWith(dayAfterSales: value));
+    });
+  }
+
+  @override
+  $ItemCopyWith<$Res> get pork {
+    return $ItemCopyWith<$Res>(_value.pork, (value) {
+      return _then(_value.copyWith(pork: value));
+    });
+  }
+
+  @override
+  $ItemCopyWith<$Res> get beef {
+    return $ItemCopyWith<$Res>(_value.beef, (value) {
+      return _then(_value.copyWith(beef: value));
+    });
+  }
+
+  @override
+  $ItemCopyWith<$Res> get chicken {
+    return $ItemCopyWith<$Res>(_value.chicken, (value) {
+      return _then(_value.copyWith(chicken: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_OrderPageStateCopyWith<$Res>
+    implements $OrderPageStateCopyWith<$Res> {
+  factory _$$_OrderPageStateCopyWith(
+          _$_OrderPageState value, $Res Function(_$_OrderPageState) then) =
+      __$$_OrderPageStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {Sales todaySales,
+      Sales tomorrowSales,
+      Sales dayAfterSales,
+      Item pork,
+      Item beef,
+      Item chicken});
+
+  @override
+  $SalesCopyWith<$Res> get todaySales;
+  @override
+  $SalesCopyWith<$Res> get tomorrowSales;
+  @override
+  $SalesCopyWith<$Res> get dayAfterSales;
+  @override
+  $ItemCopyWith<$Res> get pork;
+  @override
+  $ItemCopyWith<$Res> get beef;
+  @override
+  $ItemCopyWith<$Res> get chicken;
+}
+
+/// @nodoc
+class __$$_OrderPageStateCopyWithImpl<$Res>
+    extends _$OrderPageStateCopyWithImpl<$Res>
+    implements _$$_OrderPageStateCopyWith<$Res> {
+  __$$_OrderPageStateCopyWithImpl(
+      _$_OrderPageState _value, $Res Function(_$_OrderPageState) _then)
+      : super(_value, (v) => _then(v as _$_OrderPageState));
+
+  @override
+  _$_OrderPageState get _value => super._value as _$_OrderPageState;
+
+  @override
+  $Res call({
+    Object? todaySales = freezed,
+    Object? tomorrowSales = freezed,
+    Object? dayAfterSales = freezed,
+    Object? pork = freezed,
+    Object? beef = freezed,
+    Object? chicken = freezed,
+  }) {
+    return _then(_$_OrderPageState(
+      todaySales: todaySales == freezed
+          ? _value.todaySales
+          : todaySales // ignore: cast_nullable_to_non_nullable
+              as Sales,
+      tomorrowSales: tomorrowSales == freezed
+          ? _value.tomorrowSales
+          : tomorrowSales // ignore: cast_nullable_to_non_nullable
+              as Sales,
+      dayAfterSales: dayAfterSales == freezed
+          ? _value.dayAfterSales
+          : dayAfterSales // ignore: cast_nullable_to_non_nullable
+              as Sales,
+      pork: pork == freezed
+          ? _value.pork
+          : pork // ignore: cast_nullable_to_non_nullable
+              as Item,
+      beef: beef == freezed
+          ? _value.beef
+          : beef // ignore: cast_nullable_to_non_nullable
+              as Item,
+      chicken: chicken == freezed
+          ? _value.chicken
+          : chicken // ignore: cast_nullable_to_non_nullable
+              as Item,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_OrderPageState implements _OrderPageState {
+  const _$_OrderPageState(
+      {required this.todaySales,
+      required this.tomorrowSales,
+      required this.dayAfterSales,
+      required this.pork,
+      required this.beef,
+      required this.chicken});
+
+  @override
+  final Sales todaySales;
+  @override
+  final Sales tomorrowSales;
+  @override
+  final Sales dayAfterSales;
+  @override
+  final Item pork;
+  @override
+  final Item beef;
+  @override
+  final Item chicken;
+
+  @override
+  String toString() {
+    return 'OrderPageState(todaySales: $todaySales, tomorrowSales: $tomorrowSales, dayAfterSales: $dayAfterSales, pork: $pork, beef: $beef, chicken: $chicken)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_OrderPageState &&
+            const DeepCollectionEquality()
+                .equals(other.todaySales, todaySales) &&
+            const DeepCollectionEquality()
+                .equals(other.tomorrowSales, tomorrowSales) &&
+            const DeepCollectionEquality()
+                .equals(other.dayAfterSales, dayAfterSales) &&
+            const DeepCollectionEquality().equals(other.pork, pork) &&
+            const DeepCollectionEquality().equals(other.beef, beef) &&
+            const DeepCollectionEquality().equals(other.chicken, chicken));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(todaySales),
+      const DeepCollectionEquality().hash(tomorrowSales),
+      const DeepCollectionEquality().hash(dayAfterSales),
+      const DeepCollectionEquality().hash(pork),
+      const DeepCollectionEquality().hash(beef),
+      const DeepCollectionEquality().hash(chicken));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_OrderPageStateCopyWith<_$_OrderPageState> get copyWith =>
+      __$$_OrderPageStateCopyWithImpl<_$_OrderPageState>(this, _$identity);
+}
+
+abstract class _OrderPageState implements OrderPageState {
+  const factory _OrderPageState(
+      {required final Sales todaySales,
+      required final Sales tomorrowSales,
+      required final Sales dayAfterSales,
+      required final Item pork,
+      required final Item beef,
+      required final Item chicken}) = _$_OrderPageState;
+
+  @override
+  Sales get todaySales;
+  @override
+  Sales get tomorrowSales;
+  @override
+  Sales get dayAfterSales;
+  @override
+  Item get pork;
+  @override
+  Item get beef;
+  @override
+  Item get chicken;
+  @override
+  @JsonKey(ignore: true)
+  _$$_OrderPageStateCopyWith<_$_OrderPageState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
--- lib/UI/OrderPage/order_page.dart
+++ lib/UI/OrderPage/order_page.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:order_support/Model/Item/item.dart';
-import 'package:order_support/Provider/Store/store_provider.dart';
+import 'package:order_support/Provider/order_page_controller.dart';
 
 class OrderPage extends ConsumerWidget {
   const OrderPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final store = ref.watch(storeProvider);
-    final storeState = ref.watch(storeProvider.notifier);
+    final state = ref.watch(orderPageController);
 
     return Scaffold(
       appBar: AppBar(),
@@ -19,27 +17,14 @@ class OrderPage extends ConsumerWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ...itemList(store.items),
-              ElevatedButton(
-                onPressed: () {
-                  storeState.adjustOrder();
-                },
-                child: const Text("更新"),
-              )
+              Text("${state.pork.name}: ${state.pork.stock.dayAfter}P"),
+              Text("${state.beef.name}: ${state.beef.stock.dayAfter}P"),
+              Text("${state.chicken.name}: ${state.chicken.stock.dayAfter}P"),
+              ElevatedButton(onPressed: () {}, child: const Text("")),
             ],
           ),
         ),
       ),
     );
-  }
-
-  List<Widget> itemList(List<Item> items) {
-    final List<Widget> itemList = [];
-    for (final item in items) {
-      itemList.add(
-        Text("${item.name}: ${item.stock.dayAfter}P"),
-      );
-    }
-    return itemList;
   }
 }
